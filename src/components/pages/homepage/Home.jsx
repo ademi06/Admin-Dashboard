604b523a7c48a4685c6d76dbfca185638886aad8
ademi <bukola.tees@gmail.com>
2022-04-29 14:06:24 +0100

    creating chart commit week 2

--- src/components/pages/homepage/Home.jsx
+++ src/components/pages/homepage/Home.jsx
@@ -1,10 +1,12 @@
 import FeaturedInfo from '../../featuredInfo/FeaturedInfo'
+import Chart from '../../chart/Chart'
 import './home.css'
 
 export default function Home() {
   return (
     <div className="home">
         <FeaturedInfo/>
+        <Chart />
     </div>
   )
 }
