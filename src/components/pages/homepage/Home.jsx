1488a1e4f7c7e661be27a957cf68b823f705397c
ademi <bukola.tees@gmail.com>
2022-05-01 14:07:00 +0100

    created charts to display dummy data

--- src/components/pages/homepage/Home.jsx
+++ src/components/pages/homepage/Home.jsx
@@ -1,12 +1,15 @@
-import FeaturedInfo from '../../featuredInfo/FeaturedInfo'
-import Chart from '../../chart/Chart'
-import './home.css'
+import FeaturedInfo from '../../featuredInfo/FeaturedInfo';
+import Chart from '../../chart/Chart';
+import './home.css';
+import { userData } from '../../../dummyData';
+ 
 
 export default function Home() {
   return (
     <div className="home">
         <FeaturedInfo/>
-        <Chart />
+        <Chart data ={userData} title= "User Analytics" grid dataKey="Active User" />
     </div>
-  )
+  );
 }
+ 
