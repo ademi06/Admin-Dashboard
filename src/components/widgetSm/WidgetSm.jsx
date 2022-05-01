a2aee6be7a5628a3142a5d67b68bf889dd01d650
ademi <bukola.tees@gmail.com>
2022-05-01 15:26:05 +0100

    created a small widget to display username, picture and title

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,67 @@
+import "./widgetSm.css";
+import {Visibility} from '@mui/icons-material';
+
+export default function WidgetSm() {
+  return (
+    <div className="widgetSm">
+        <span className="widgetSmTitle">New Join Members</span>
+        <ul className="widgetSmList">
+            <li className="widgetSmListItem">
+                <img src="https://avatars.githubusercontent.com/u/72165670?s=400&u=e9f0eeb9ee91e54899d974c551b397c7410abf8a&v=4" alt="Bukoka" className="widgetSmImage" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername"> Bukola Fatunde</span>
+                    <span className="widgetSmUserTitle"> Software Engineer</span> 
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility className="widgetSmIcon" />
+                    Display
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://avatars.githubusercontent.com/u/72165670?s=400&u=e9f0eeb9ee91e54899d974c551b397c7410abf8a&v=4" alt="Bukoka" className="widgetSmImage" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername"> Bukola Fatunde</span>
+                    <span className="widgetSmUserTitle"> Software Engineer</span> 
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility className="widgetSmIcon" />
+                    Display
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://avatars.githubusercontent.com/u/72165670?s=400&u=e9f0eeb9ee91e54899d974c551b397c7410abf8a&v=4" alt="Bukoka" className="widgetSmImage" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername"> Bukola Fatunde</span>
+                    <span className="widgetSmUserTitle"> Software Engineer</span> 
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility className="widgetSmIcon" />
+                    Display
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://avatars.githubusercontent.com/u/72165670?s=400&u=e9f0eeb9ee91e54899d974c551b397c7410abf8a&v=4" alt="Bukoka" className="widgetSmImage" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername"> Bukola Fatunde</span>
+                    <span className="widgetSmUserTitle"> Software Engineer</span> 
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility className="widgetSmIcon" />
+                    Display
+                </button>
+            </li>
+            <li className="widgetSmListItem">
+                <img src="https://avatars.githubusercontent.com/u/72165670?s=400&u=e9f0eeb9ee91e54899d974c551b397c7410abf8a&v=4" alt="Bukoka" className="widgetSmImage" />
+                <div className="widgetSmUser">
+                    <span className="widgetSmUsername"> Bukola Fatunde</span>
+                    <span className="widgetSmUserTitle"> Software Engineer</span> 
+                </div>
+                <button className="widgetSmButton">
+                    <Visibility className="widgetSmIcon" />
+                    Display
+                </button>
+            </li>
+        </ul>
+    </div>
+  )
+}
