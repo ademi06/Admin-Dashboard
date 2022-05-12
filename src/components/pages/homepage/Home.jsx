e9e67083aad4b2007f86fbf4da80b1d3c5d24364
ademi <bukola.tees@gmail.com>
2022-05-12 20:06:29 +0100

    added a slider to the homepage

--- src/components/pages/homepage/Home.jsx
+++ src/components/pages/homepage/Home.jsx
@@ -4,18 +4,38 @@ import './home.css';
 import { userData } from '../../../dummyData';
 import WidgetSm from '../../widgetSm/WidgetSm';
 import WidgetLg from '../../widgetLg/WidgetLg';
+import VerticalSlider from '../../verticalSlider/VerticalSlider';
+import { useState } from 'react';
  
 
-export default function Home() {
+const Home = () => {
+  const [getValue, setGetValue] = useState(1);
+  
+
+  let newValue = {};
+  let sliderValue =getValue ;
+  const transformedData = userData.map(calculatedNewMonth);
+  function calculatedNewMonth(month){
+    const newActiveUserValue = sliderValue * month["Active User"];
+    newValue = {...month, "Active User": newActiveUserValue}
+    return newValue;
+  }
+
   return (
     <div className="home">
         <FeaturedInfo/>
-        <Chart data ={userData} title= "User Analytics" grid dataKey="Active User" />
+        <div className="homeContainer">
+        <VerticalSlider 
+            changedValue = {getValue => setGetValue(getValue)} 
+      />
+        <Chart data ={transformedData} title= "User Analytics" grid dataKey="Active User" />
+        </div>
         <div className="homeWidgets">
           <WidgetSm />
           <WidgetLg />
         </div>
     </div>
   );
-}
- 
+  }
+
+export default Home;
---
+++ src/components/verticalSlider/VerticalSlider.jsx
@@ -0,0 +1,45 @@
+import "./verticalSlider.css"
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+
+
+const VerticalSlider = (props) =>  {
+
+  const [value, setValue] = useState(1);
+  
+  const newSliderValue = (event, value) => {
+  setValue(value);
+  }
+
+  function PreventHorizontalKeyboardNavigation(event: React.KeyboardEvent) {
+    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+      event.preventDefault();
+    }
+  }
+
+  return ( <div className="verticlalSlider">
+    <Box sx={{ height: 150 }}>
+      <Slider
+        sx={{
+          '& input[type="range"]': {
+            WebkitAppearance: 'slider-vertical',
+          },
+        }}
+        orientation="vertical"
+        defaultValue={10}
+        step={1}
+        min={1} 
+        max={10}
+        value={value}
+        onChange={newSliderValue}
+        onClick={() => props.changedValue(value)}
+        aria-label="Temperature"
+        onKeyDown={PreventHorizontalKeyboardNavigation}
+      />
+    </Box>
+    </div>
+  );
+}
+export default VerticalSlider;
